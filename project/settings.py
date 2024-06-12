d2f4ee95cc5e594f8f9b28d0b64170ba7f189ff6
Shauryaman Sawai <sawaisushil@gmail.com>
2024-06-12 12:39:43 +0530

    com lates

--- project/settings.py
+++ project/settings.py
@@ -78,23 +78,16 @@ WSGI_APPLICATION = "project.wsgi.application"
 LOGOUT_REDIRECT_URL = '/logout'
 
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Library',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
-# settings.py
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
